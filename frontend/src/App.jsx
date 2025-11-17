47e54bd97fc47d4f9b1ce1d04f27ba0dd923dc52
Matthew Sanner <matthewsannerdev@gmail.com>
2025-11-17 17:24:03 -0600

    nav troubleshooting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ import EditEvent from "./pages/EditEvent";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
-// import ErrorBoundary from "./components/ErrorBoundary";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
   return (
@@ -23,26 +23,26 @@ function App() {
       <Router>
         <Navbar />
         <div className="p-4">
-          {/* <ErrorBoundary> */}
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/items" element={<Items />} />
-            <Route path="/items/:id" element={<ItemDetail />} />
-            <Route path="/items/new" element={<NewItem />} />
-            <Route path="/items/:id/edit" element={<EditItem />} />
-            <Route path="/items/:id/book" element={<NewItemBooking />} />
-            <Route
-              path="/itembookings/:id/edit"
-              element={<EditItemBooking />}
-            />
-            <Route path="/events" element={<Events />} />
-            <Route path="/events/:id" element={<EventDetail />} />
-            <Route path="/events/new" element={<NewEvent />} />
-            <Route path="/events/:id/edit" element={<EditEvent />} />
-          </Routes>
-          {/* </ErrorBoundary> */}
+          <ErrorBoundary>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/items" element={<Items />} />
+              <Route path="/items/:id" element={<ItemDetail />} />
+              <Route path="/items/new" element={<NewItem />} />
+              <Route path="/items/:id/edit" element={<EditItem />} />
+              <Route path="/items/:id/book" element={<NewItemBooking />} />
+              <Route
+                path="/itembookings/:id/edit"
+                element={<EditItemBooking />}
+              />
+              <Route path="/events" element={<Events />} />
+              <Route path="/events/:id" element={<EventDetail />} />
+              <Route path="/events/new" element={<NewEvent />} />
+              <Route path="/events/:id/edit" element={<EditEvent />} />
+            </Routes>
+          </ErrorBoundary>
         </div>
       </Router>
     </AuthProvider>
